bc6423f3eb70d5d2ceb03f54858c7b7a4f72fbef
Marcin Pawlus <marcinpawlus97@gmail.com>
2021-08-15 12:42:10 +0200

    Create new files and import

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,10 +1,31 @@
+import React from 'react';
 import '../styles/App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Header from './Header';
+import Navigation from './Navigation';
+import Page from './Page';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          {<Header/>}
+        </header>
+        <main>
+          <aside>
+            {<Navigation/>}
+          </aside>
+          <section className='page'>
+            {<Page/>}
+          </section>
+        </main>
+        <footer>
+          {<Footer/>}
+        </footer>
+      </div>
+    </Router>
   );
 }
 
