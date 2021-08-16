a8f234ccb492948f6b64edc46bba75a95ecb77ce
Marcin Pawlus <marcinpawlus97@gmail.com>
2021-08-16 13:40:20 +0200

    Finish navigation structure and add css

--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
+import '../styles/Navigation.css';
+
+const list = [
+    {name: "start", path: "/", exact: true},
+    {name: "postacie", path: "/characters"},
+    {name: "wydarzenia", path: "/events"},
+    {name: "kontakt", path: "/contact"},
+];
 
 const Navigation = () => {
+
+    const menu = list.map(item => (
+        <li key = {item.name}>
+            <NavLink to={item.path} exact={item.exact ? item.exact : false}>{item.name}</NavLink>
+        </li>
+    ))
+
     return ( 
-        <div>Nav</div>
+        <nav className="main">
+            <ul>
+               {menu}
+            </ul>
+        </nav>
      );
 }
  
