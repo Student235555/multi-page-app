38cddd9b3d35f22d07a7014ad4474c7dc9184e6d
Marcin Pawlus <marcinpawlus97@gmail.com>
2021-08-18 13:34:01 +0200

    Add all subpages components

---
+++ src/pages/CharactersPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CharactersPage = () => {
+    return ( 
+        <div>Postacie</div>
+     );
+}
+ 
+export default CharactersPage;
---
+++ src/pages/EventsPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EventsPage = () => {
+    return ( 
+        <div>Wydarzenia</div>
+     );
+}
+ 
+export default EventsPage;
