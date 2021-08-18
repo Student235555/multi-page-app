86bf2e8dc7c38c8e3bb5ccd97f790b3e5fff6efc
Marcin Pawlus <marcinpawlus97@gmail.com>
2021-08-18 13:33:40 +0200

    Finish main page.js component

--- src/layouts/Page.js
+++ src/layouts/Page.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+import HomePage from '../pages/HomePage';
+import CharactersPage from '../pages/CharactersPage';
+import EventsPage from '../pages/EventsPage';
+import ContactPage from '../pages/ContactPage';
+import ErrorPage from '../pages/ErrorPage';
 
 const Page = () => {
-    return ( 
-        <div>Page</div>
+    return (
+        <>
+            <Switch>
+                <Route path='/' exact component={HomePage}/>
+                <Route path='/characters' component={CharactersPage}/>
+                <Route path='/events' component={EventsPage}/>
+                <Route path='/contact' component={ContactPage}/>
+                <Route component={ErrorPage}/>
+            </Switch>
+        </>
      );
 }
- 
+
 export default Page;
