a460a6906a8389393d0c1408a4256102f2e35e94
Marcin Pawlus <marcinpawlus97@gmail.com>
2021-08-22 13:53:57 +0200

    Make ContactPage structure, look and handles

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import '../styles/ContactPage.css';
 
 const ContactPage = () => {
+
+    const [messageContent, setMessageContent] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setMessageContent('');
+    }
+
     return ( 
-        <div>Kontakt</div>
+        <div className='contact'>
+            <form onSubmit={handleSubmit}>
+                <h3>Napisz do nas</h3>
+                <textarea 
+                    value={messageContent} 
+                    onChange={event => setMessageContent(event.target.value)}
+                    placeholder='Wpisz wiadomość...'
+                ></textarea>
+                <button>Wyślij</button>
+            </form>
+        </div>
      );
 }
  
