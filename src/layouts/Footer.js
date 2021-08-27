f844634be87219bffbc4d80a709225749cc1f2f7
Marcin Pawlus <marcinpawlus97@gmail.com>
2021-08-27 14:13:45 +0200

    Update footer

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import '../styles/Footer.css';
 
 const Footer = () => {
     return ( 
-        <div>Footer</div>
+        <div className='footer'>
+             <p class="page footer_text">Copyright &copy; 2021 <span>Marcin Pawluś</span></p>
+        </div>
      );
 }
  
