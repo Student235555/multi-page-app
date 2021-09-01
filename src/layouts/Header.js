c38db405eecd69df9d4763ba70e36d5cce04bcc7
Marcin Pawlus <marcinpawlus97@gmail.com>
2021-09-01 16:13:29 +0200

    Update header

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom'
 import '../styles/Header.css';
-import img1 from '../images/header1.jpg';
-import img2 from '../images/header2.jpg';
-import img3 from '../images/header3.jpg';
-import img4 from '../images/header4.jpg';
-import img5 from '../images/header5.jpg';
+import img1 from '../images/header3.jpg';
+import img3 from '../images/header16.jpg';
+import img4 from '../images/header8.jpg';
+import img7 from '../images/header24.jpg';
+import img8 from '../images/header25.jpg';
 
 const Header = () => {
     return ( 
         <>
             <Switch> {/*jeśli jakikolwiek Route po drodze jest prawdziwy to kończy jego działanie*/}
                 <Route path='/' exact render= {() => (
-                    <img src={img3} alt='Biblioteka'/>
+                    <img src={img7} alt='Rzeźby ścienne'/>
                 )}/>
                 <Route path='/characters' render= {() => (
-                    <img src={img1} alt='Biblioteka'/>
+                    <img src={img8} alt='Rzeźby ścienne'/>
                 )}/>
-                <Route path='/events' render= {() => (
-                    <img src={img2} alt='Biblioteka'/>
+                <Route path='/gods' render= {() => (
+                    <img src={img3} alt='Posągi'/>
                 )}/>
                 <Route path='/contact' render= {() => (
-                    <img src={img4} alt='Biblioteka'/>
+                    <img src={img4} alt='Maski na ścianie'/>
                 )}/>
                 <Route render= {() => (
-                    <img src={img5} alt='Biblioteka'/>
+                    <img src={img1} alt='Biblioteka'/>
                 )}/>
             </Switch>
         </>
